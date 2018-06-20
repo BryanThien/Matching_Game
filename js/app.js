4ddecf4e0b12598ef84dbfdcf3f4d5dc069d25f8
Bryan Thien <eonearthproductions@gmail.com>
2018-06-19 20:38:47 -0500

    Feature: Adds Timer that can restart

--- js/app.js
+++ js/app.js
@@ -10,13 +10,33 @@ let secondClick = "";
 let clicks = 0;
 let winner = 0;
 let cardLock = "off";
-
+let minutesLabel = document.getElementById("minutes");
+let secondsLabel = document.getElementById("seconds");
+let totalSeconds = 0;
 
 startRound();
  
 /* Functions*/
 
+/* Timer */
+setInterval(setTime, 1000);
+function setTime() {
+    ++totalSeconds;
+    secondsLabel.innerHTML = pad(totalSeconds % 60);
+    minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+}
+
+function pad(val) {
+    let valString = val + "";
+    if (valString.length < 2){
+        return "0" + valString;
+    } else {
+        return valString;
+    }
+}
+
 function startRound() {
+    totalSeconds = 0;
     winner = 0;
     clicks = 0;
     document.querySelector(".moves").innerHTML = 0;
